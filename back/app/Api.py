7e915a7d30aabf23365bbf41fea385d901255693
Damien Rochat <rochat.damien@gmail.com>
2019-06-10 16:02:22 +0200

    redo arxiv parser

--- back/app/Api.py
+++ back/app/Api.py
@@ -12,12 +12,7 @@ api = Api(app)
 
 graph = Graph('bolt://neo4j:7687/db/data')
 
-
-class Search(Resource):
-
-    def searchData(self, search):
-
-        query = """
+query = """
 MATCH (pub:Publication)-[]-(rel), (from:Publication {id: {pub}})
 WITH {item: id(pub), categories: collect(id(rel))} as pubData, [id(from)] as sourceIds
 WITH collect(pubData) as data, sourceIds
@@ -31,23 +26,76 @@ ORDER BY similarity DESC
 LIMIT 50
 """
 
-        results = graph.run(query, pub=search)
-        return [
-            {
-                'id': row['id'],
-                'title': row['title'],
-                'summary': row['summary'],
-                'authors': row['authors'].split(','),
-                'affiliations': [],
-                'pdf_link': row['pdf_link'],
-                'primary_category': [],
-                'categories': row['categories'].split(','),
-                'documents': {'type': 'pdf'},
-                'similarity': row['similarity'],
-            }
-            for row in results
-        ]
+def fetch_neo4j_articles(pub):
+    return [
+        {
+            'id': row['id'],
+            'title': row['title'],
+            'summary': row['summary'],
+            'authors': row['authors'].split(','),
+            'affiliations': [],
+            'pdf_link': row['pdf_link'],
+            'primary_category': [],
+            'categories': row['categories'].split(','),
+            'documents': {'type': 'pdf'},
+            'similarity': row['similarity'],
+        }
+        for row in graph.run(query, pub=pub)
+    ]
 
+
+def fetch_arxiv(search):
+    arxiv_id_regex = "^\d{4}\."
+    arxiv_id = re.search(arxiv_id_regex, search)
+    
+    #if arxiv_url:
+    if arxiv_id:
+        paper_id = search
+        base_url = 'http://export.arxiv.org/api/query?search_query='
+        data = urlopen(base_url + paper_id, None, timeout=120).read()
+        parsed = ET.fromstring(data)
+        parsed_list = parse_to_list(parsed)
+        
+        arxiv_link = parsed_list[0][0]
+        arxiv_title = sanitize_str(parsed_list[0][3])
+        arxiv_summary = sanitize_str(parsed_list[0][4])
+        arxiv_author = parsed_list[0][5]
+        arxiv_pdf = paper_id
+        arxiv_primary_category = parsed_list[0][10]
+        arxiv_categories = parsed_list[0][11]
+        
+        print("arxiv_link",arxiv_link)
+        print("arxiv_title",arxiv_title)
+        print("arxiv_summary",arxiv_summary)
+        print("arxiv_author",arxiv_author)
+        print("arxiv_pdf",arxiv_pdf)
+        print("arxiv_primary_categories",arxiv_primary_category)
+        print("arxiv_categories",arxiv_categories)
+
+        return [{
+            "id":arxiv_link,
+            "idPage": paper_id,
+            "updated": "updated",
+            "published": "published",
+            "title": arxiv_title,
+            "summary": arxiv_summary,
+            "authors": arxiv_author,
+            "affiliations": "affiliations",
+            "doi": "doi",
+            "journal_ref": "journal_ref",
+            "pdf_link": arxiv_pdf,
+            "primary_category": arxiv_primary_category,
+            "categories": arxiv_categories,
+            "documents": [{"type": "pdf"}],
+        }]
+    
+    return None
+
+
+class Search(Resource):
+
+    def searchData(self, search):
+        return fetch_neo4j_articles(search)
 
     def get(self):
         parser = reqparse.RequestParser()
